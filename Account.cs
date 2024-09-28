ba8202eb6a8348688b9e9b0b37f96ebe99626fd2
Fidan <fidan@MacBook-Pro-Fidan.local>
2024-09-28 13:04:38 +0400

    first commit

---
+++ Account.cs
@@ -0,0 +1,15 @@
+﻿namespace String_Array_Class_Intro
+{
+	public class Account
+	{
+		public void Login(string username,string password)
+		{
+			if (username == "lorem123" && password == "123456*")
+				Console.WriteLine("You have successfully logged in");
+			else
+				Console.WriteLine("Error! Try again");
+
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace String_Array_Class_Intro;
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            string reverseSentence = ReverseSentence("Salam necesen");
+            Console.WriteLine(reverseSentence);
+
+            Account login = new Account();
+
+            Console.WriteLine("Enter username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+            login.Login(username, password);
+        }
+
+        #region Task 1
+        static string ReverseSentence(string sentence)
+
+        {
+            string reversed = "";
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                reversed = sentence[i] + reversed;
+            }
+            return reversed;
+        }
+        #endregion
+
+    }
+}
+
